8dd57cc4dec1b2017bce31cb959ef005fdb7bc7c
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-08 14:54:30 +0530

    Initial Commit

---
+++ src/main/java/com/payment/CoursePayment/CoursePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.payment.CoursePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/CoursePayment/CoursePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.CoursePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
