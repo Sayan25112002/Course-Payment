324841eb744741232693737a94459f718905623c
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-08 15:07:33 +0530

    CoursePayment.jrxml Modified

---
+++ src/main/java/com/payment/CoursePayment/entity/CourseDetails.java
@@ -0,0 +1,4 @@
+package com.payment.CoursePayment.entity;
+
+public class CourseDetails {
+}
--- src/main/java/com/payment/CoursePayment/entity/StudentDetail.java
+++ src/main/java/com/payment/CoursePayment/entity/StudentDetail.java
@@ -1,4 +1,28 @@
 package com.payment.CoursePayment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class StudentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String universityName;
+
+    private String universityLocation;
+
+    private String semesterName;
+
+    private String name;
+
+
 }
